2bf12bd7e031f445a4d9be72180157526ec79c57
Tawfeeq Amro <tawfeekamr@Tawfeeqs-MacBook-Pro.local>
2021-04-25 04:50:01 +0300

    add functions

--- src/App.js
+++ src/App.js
@@ -4,38 +4,72 @@ import RomanNumerals from './services/RomanNumerals'
 import {useState} from "react";
 
 function App() {
-  const defaultDecimal = 1000;
-  const defaultRoman = RomanNumerals.toRoman(defaultDecimal)
-  const [romanNumber, setRomanNumber] = useState(defaultRoman);
-  const [decimalNumber, setDecimalNumber] = useState(defaultDecimal);
 
-  const onRomanNumberChange = event => {
-    const romanNumber = event.target.value.toUpperCase();
-    setRomanNumber(romanNumber);
 
-  };
+    const defaultDecimal = 111;
+    const defaultRoman = RomanNumerals.toRoman(defaultDecimal);
+    const [romanNumber, setRomanNumber] = useState(defaultRoman);
+    const [romanNumberValid, setRomanNumberValid] = useState(RomanNumerals.isRomanNumberValid(defaultRoman));
+    const [decimalNumber, setDecimalNumber] = useState(defaultDecimal);
+    const [decimalNumberValid, setDecimalNumberValid] = useState(RomanNumerals.isRomanNumberValid(defaultRoman));
 
-  const onDecimalNumberChange = event => {
-    const decimalNumber = event.target.value;
-    setDecimalNumber(decimalNumber);
-  };
+    const onRomanNumberHandler = e => {
+        const romanNumber = e.target.value.toUpperCase();
+        setRomanNumber(romanNumber);
+        setRomanNumberValid(RomanNumerals.isRomanNumberValid(romanNumber));
+        if (RomanNumerals.isRomanNumberValid(romanNumber)) {
+            const decimal = RomanNumerals.fromRoman(romanNumber);
+            setDecimalNumber(decimal);
+            setDecimalNumberValid(true);
+        } else {
+            setDecimalNumber("");
+        }
+    };
+
+    const onDecimalNumberHandler = e => {
+        const decimalNumber = e.target.value;
+        if (RomanNumerals.isDecimalNumberValid(decimalNumber)) {
+            setDecimalNumberValid(true);
+            setRomanNumberValid(true);
+            setRomanNumber(RomanNumerals.toRoman(decimalNumber));
+        } else {
+            setDecimalNumberValid(false);
+            setRomanNumber("");
+        }
+        setDecimalNumber(decimalNumber);
+    };
   return (
     <div className="App">
+        <label htmlFor=""> Roman </label>
       <input
           type="text"
+          className={romanNumberValid ? 'valid' : 'not-valid'}
           placeholder="Enter roman number"
           value={romanNumber}
-          onChange={onRomanNumberChange}
+          onChange={onRomanNumberHandler}
           required
       />
-      <input
+      <p
+          className={romanNumberValid ? 'd-none' : 'not-valid-text'}
+      >
+          Not valid Roman number, Please enter correct Roman character not exceed MMM
+      </p>
+      <br/>
+        <label htmlFor=""> Number </label>
+        <input
           type="number"
+          className={decimalNumberValid ? 'valid' : 'not-valid'}
           placeholder="Enter decimal number"
           value={decimalNumber}
-          onChange={onDecimalNumberChange}
+          onChange={onDecimalNumberHandler}
           pattern="\d*"
           required
       />
+        <p
+            className={decimalNumberValid ? 'd-none' : 'not-valid-text'}
+        >
+            Not valid number, please make sure you're writing numbers not exceed 2000
+        </p>
     </div>
   );
 }
--- src/services/RomanNumerals.js
+++ src/services/RomanNumerals.js
@@ -2,6 +2,9 @@ import romanLetters from '../helpers';
 
 let RomanNumerals = {};
 
+ const MIN_DECIMAL_VALUE = 1;
+ const MAX_DECIMAL_VALUE = 2000;
+
 export default RomanNumerals = {
 
     // Form Decimal to Roman
@@ -17,21 +20,33 @@ export default RomanNumerals = {
     },
 
 
-// Form Roman to Decimal
-    fromRoman: (romanNumber) => {
-        return this.fromRomanConverter(romanNumber, 0);
-    },
-
-    fromRomanConverter: (romanNumber, decimalValue) => {
+    // Form Roman to Decimal
+    fromRoman: function (romanNumber, decimalValue = 0) {
 
         for (let key in romanLetters) {
             const value = romanLetters[key];
             if (romanNumber.indexOf(key) === 0) {
-                decimalValue = value + this.fromRomanConverter(romanNumber.substr(key.length), decimalValue);
+                decimalValue = value + this.fromRoman(romanNumber.substr(key.length), decimalValue);
                 return decimalValue;
             }
         }
         return decimalValue;
+    },
+
+    // Check if the Roman valid
+    isRomanNumberValid: function(romanNumber) {
+        return /^(?=[MDCLXVI])(M{0,3})(C[MD]|D?C{0,3})(X[CL]|L?X{0,3})(I[XV]|V?I{0,3})$/.test(
+            romanNumber
+        );
+    },
+
+    // Check if the Number valid
+     isDecimalNumberValid: (decimalNumber) => {
+        return (
+            /^\d+$/.test(decimalNumber) &&
+            decimalNumber >= MIN_DECIMAL_VALUE &&
+            decimalNumber <= MAX_DECIMAL_VALUE
+        );
     }
 
 }
